62418a86a52a08e56081253b148ca7f57f1f24cd
Steven Ortiz <sortiz832@gmail.com>
2021-03-26 17:38:47 +0100

    Updated threads

--- src/app/shared/thread/thread.component.ts
+++ src/app/shared/thread/thread.component.ts
@@ -10,7 +10,35 @@ export class ThreadComponent {
   @Input() board: string;
   @Input() thread: Thread;
 
+  get image(): string {
+    return `${this.url}/${this.board}/${this.thread.tim}${this.thread.ext}`;
+  }
+
+  get name(): string {
+    return this.thread.name;
+  }
+
+  get text(): string {
+    return this.thread.com;
+  }
+
   get thumbnail(): string {
-    return `http://i.4cdn.org/${this.board}/${this.thread.tim}s.jpg`;
+    return `${this.url}/${this.board}/${this.thread.tim}s.jpg`;
+  }
+
+  get time(): number {
+    return this.thread.tim;
+  }
+
+  get title(): string {
+    return this.thread.sub ?? `No. ${this.thread.no}`;
+  }
+
+  get url(): string {
+    return 'https://i.4cdn.org';
+  }
+
+  onClick(event: MouseEvent): void {
+    event.stopImmediatePropagation();
   }
 }
