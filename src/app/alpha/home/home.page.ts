523fbd564f0d5054b10a36aa33088a34d2d26450
Steven Ortiz <sortiz832@gmail.com>
2021-03-27 01:19:42 +0100

    Added animation

--- src/app/alpha/home/home.page.ts
+++ src/app/alpha/home/home.page.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, ViewChild } from '@angular/core';
 import { IonContent, ViewWillEnter } from '@ionic/angular';
 import { Observable } from 'rxjs';
@@ -7,13 +8,25 @@ import { Api } from '../../services/api.service';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger(
+      'fab',
+      [
+        state('true', style({ transform: 'scale(0)' })),
+        state('false', style({ transform: 'scale(1)' })),
+        transition('* <=> *', animate('250ms ease-in-out')),
+      ],
+    ),
+  ],
 })
 export class HomePage implements ViewWillEnter {
   @ViewChild(IonContent) content: IonContent;
   board: string;
   catalog$: Observable<unknown>;
+  isFabHidden = true;
 
   constructor(private api: Api) {
+    globalThis.temp1 = this;
   }
 
   ionViewWillEnter(): void {
@@ -22,6 +35,14 @@ export class HomePage implements ViewWillEnter {
     }
     if (!this.catalog$) {
       this.catalog$ = this.api.getCatalog(this.board);
+    }
+  }
+
+  onScroll(event: CustomEvent): void {
+    if (event.detail.deltaY > 0) {
+      this.isFabHidden = true;
+    } else if (event.detail.deltaY < 0) {
+      this.isFabHidden = false;
     }
   }
 
