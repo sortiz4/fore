9b532f84f35002da05dfc8dc293a18500cedc6a2
Steven Ortiz <sortiz832@gmail.com>
2021-06-10 21:08:51 -0500

    Change help location

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   api: 'https://a.4cdn.org',
   cdn: 'https://i.4cdn.org',
-  help: 'https://gitlab.com/sortiz4/fore/-/issues',
+  help: 'https://gitlab.com/sortiz4/fore',
   version: '2021.06.10',
   production: true,
 };
