3a8d02fbd7c164352d6421e12b4383e7f15f1baa
Steven Ortiz <sortiz832@gmail.com>
2021-07-19 13:34:48 -0500

    Updated environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
+  production: false,
+  version: '2021.06.10',
   api: '/api',
   cdn: 'https://i.4cdn.org',
   help: 'https://gitlab.com/sortiz4/fore',
-  version: '2021.06.10',
-  production: false,
 };
